a2bb4d87850c034cc2b9d0842ed598cf7e8062a4
Oxana Theis <otheis@protonmail.com>
2022-06-04 23:52:39 +0200

    version avec home page et recup de données depuis json

---
+++ src/components/Property/index.jsx
@@ -0,0 +1,16 @@
+
+//import "../../styles/Footer.css"
+//  <img src={logo} alt="Kasa" className="nav-logo"/>
+
+function Property(props) {
+  return (
+    <div>
+       
+      <img alt="Kasa" className="nav-logo"/>
+     <h3>{props.title}</h3>
+
+    </div>
+  );
+}
+
+export default Property;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,47 @@
+import React, { useEffect} from "react";
 import Header from "../../components/Header";
 import Banner from "../../components/Banner";
+import Property from "../../components/Property";
+//import Footer from "../../components/Footer";
+
 function Home() {
+  //? ou mettre .json -dossier public , ne fonctionnait pas dans src?
+
+  const linkToJson = "data/logements.json";
+  /*
+  const [test, setTest] = useState({})
+  setTest(data)
+  console.log(setTest(data))*/
+
+
+ // useEffect(() => {
+    fetch(linkToJson)
+      .then(function (response) {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then(function (data) {
+        console.log(data);
+
+        for (let property of data) {
+          console.log(property);
+
+        }
+      
+
+      })
+
+      .catch(function (error) {
+        console.log(error);
+      });
+  //}, []);
+
   return (
     <div className="App">
-     <Header></Header>
-     <Banner></Banner>
-  
-      
+      <Header></Header>
+      <Banner></Banner>
+      <Property></Property>
     </div>
   );
 }
